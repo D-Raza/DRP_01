0dfb48e7f3048ec9e75766bb10cabd49b86340ab
Danish Raza <danish.raza21@imperial.ac.uk>
2023-06-09 07:06:52 +0100

    dr421:<feature> Draft commit of proxy put request

--- drpproject/drpapp/urls.py
+++ drpproject/drpapp/urls.py
@@ -5,10 +5,15 @@ from . import views
 urlpatterns = [
     # ex: /drpapp/ - the index page
     path("", views.index, name="index"),
+    
     # ex: /drpapp/comparison/ - the price comparison page
     path("comparison/", views.comparison, name="comparison"),
+    
     # ex: /drpapp/diet/ - the dietary preferences page
     path("diet/", views.diet, name="diet"),
+    
     # ex: /drpapp/recommendations/ - the recipe recommendations page
     path("recommendations/", views.recommendations, name="recommendations"),
+
+    path("proxy_tesco_basket/", views.proxy_tesco_basket, name="proxy_tesco_basket"),
 ]
